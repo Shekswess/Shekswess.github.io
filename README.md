446487ac70b2bbe31b3dd92a4cd4c773ffc81c0d
Bojan Jakimovski <bokicasheks@yahoo.com>
2021-12-03 19:35:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 id="title" align="center">Portfolio Website</h1>
+
+<h3 align="center">:desktop_computer: A Portfolio Website that looks and acts like a Terminal :computer:</h3>
---
+++ js/data.js
@@ -0,0 +1,19 @@
+var data = {
+  bio: [
+    {name: 'Name', value: 'Bojan Jakimovski'},
+    {name: 'Email', value: 'jakimovski_bojan@outlook.com'},
+    {name: 'Phone', value: '+389 72 264 151'},
+    {name: 'Country', value: 'Macedonia'},
+    {name: 'City', value: 'Skopje'}
+  ],
+  education: [
+    {name: 'High School', value: 'SUGS Koco Racin, Gymnazium, Mathematics and Computer Science direction'},
+    {name: 'University', value: 'Ss. Cyril and Methodius University, Faculty of Electrical Engineering & Information Technologies'}
+  ],
+  resume: 'BojanJakimovski.pdf',
+  awards: 'Awards.pdf',
+  socials: [
+    {name: 'LinkedIn', value: 'https://linkedin.com/in/bojan-jakimovski-a5224b205/'},
+    {name: 'GitHub', value: 'https://github.com/Shekswess'}
+  ]
+}
