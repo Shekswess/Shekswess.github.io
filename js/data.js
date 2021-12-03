20f0ec997831b11709bb88fbf05882d605431249
Bojan Jakimovski <bokicasheks@yahoo.com>
2021-12-03 19:53:27 +0100

    changed typo

--- js/data.js
+++ js/data.js
@@ -11,7 +11,7 @@ var data = {
     {name: 'University', value: 'Ss. Cyril and Methodius University, Faculty of Electrical Engineering & Information Technologies'}
   ],
   resume: 'BojanJakimovski.pdf',
-  awards: 'Awards.pdf',
+  awards: 'awards.pdf',
   socials: [
     {name: 'LinkedIn', value: 'https://linkedin.com/in/bojan-jakimovski-a5224b205/'},
     {name: 'GitHub', value: 'https://github.com/Shekswess'}
