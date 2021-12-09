d5742de6b457c60224f62f2d3c81e7e5628f2bbc
Bojan Jakimovski <bokicasheks@yahoo.com>
2021-12-09 16:17:48 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -10,7 +10,7 @@ var data = {
     {name: 'High School', value: 'SUGS Koco Racin, Gymnazium, Mathematics and Computer Science direction'},
     {name: 'University', value: 'Ss. Cyril and Methodius University, Faculty of Electrical Engineering & Information Technologies'}
   ],
-  resume: 'BojanJakimovski.pdf',
+  resume: 'Bojan.pdf',
   awards: 'awards.pdf',
   socials: [
     {name: 'LinkedIn', value: 'https://linkedin.com/in/bojan-jakimovski-a5224b205/'},
